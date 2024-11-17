ebccf3119bef580eacb5f5a5c69c478c22b5b855
Mimanshu Sharma 7 <mimanshusharmatrp1927@gmail.com>
2024-11-17 14:55:35 +0530

    first commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { navItems } from '@/constants';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react'
+
+interface Props {
+    fullName: string;
+    avatar: string;
+    email: string;
+  }
+
+const Sidebar = ({fullName, avatar, email}:Props) => {
+    const pathname= usePathname();
+
+  return (
+    <aside className="sidebar">
+    <Link href="/">
+    <Image src="/assets/icons/Data Nest (7)full.svg" alt="logo" width={260} height={90} className="hidden h-auto lg:block" />
+
+    <Image src="/assets/icons/Data Nest (6)half.svg" alt="logo" width={119} height={119} className="lg:hidden"/>
+
+    </Link>
+
+    <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+            { navItems.map(({url, name, icon})=> (
+                     <Link key={name} href={url} className="lg:w-full">
+                        <li className={cn("sidebar-nav-item", pathname ===url && "shad-active",)}>
+                            <Image src={icon} alt={name} width={24} height={24} className={cn("nav-icon", pathname=== url && "nav-icon-active", )}
+                            />
+                            <p className='hidden lg:block'>{name}</p>
+                        </li>
+                     </Link>
+                 ))}
+            
+        </ul>
+    </nav>
+
+    <Image
+    src="/assets/images/files-2.png"
+    alt="logo"
+    width={506}
+    height={418}
+    className="w-full"
+  />
+
+  <div className="sidebar-user-info">
+    <Image
+      src={avatar}
+      alt="Avatar"
+      width={54}
+      height={54}
+      className="sidebar-user-avatar"
+    />
+    <div className="hidden lg:block">
+      <p className="subtitle-2 capitalize">{fullName}</p>
+      <p className="caption">{email}</p>
+    </div>
+  </div>
+</aside>
+  );
+};
+
+export default Sidebar
