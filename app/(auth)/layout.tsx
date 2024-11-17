301cead69437786568faae811961c9617f0eda82
Mimanshu Sharma 7 <mimanshusharmatrp1927@gmail.com>
2024-11-17 15:40:18 +0530

    fix deployment

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
     <div className="flex min-h-screen">
         <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
             <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
-                <Image src="/assets/icons/datanest8.svg" alt="logo" width={304} height={85} className="h-auto" />
+                <Image src="/assets/icons/Data Nest (7)full.svg" alt="logo" width={304} height={85} className="h-auto" />
 
                 <div className="space-y-5 text-white">
                     <h1 className='h1'>
@@ -23,7 +23,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
 
         <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
             <div className="mb-16 lg:hidden">
-                <Image src="/assets/icons/logo-full-brand.svg" alt="logo" width={224} height={82} className="h-auto w-[200px] lg:w-[250px]" />
+                <Image src="/assets/icons/Data Nest (6)half.svg" alt="logo" width={224} height={82} className="h-auto w-[200px] lg:w-[250px]" />
             </div>
         {children}
         </section>
